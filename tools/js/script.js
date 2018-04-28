f2e0407497de4b245d741d008312a8b5aa5b1ff8
tanandre <tanneman1@gmail.com>
2018-04-27 23:28:56 -0400

    add vuetify move buttons and texarea to vuetify components

--- tools/js/script.js
+++ tools/js/script.js
@@ -1,92 +1,96 @@
-Vue.use(VueMaterial);
+function copyToClipboard () {
+	setTimeout(() => {
+		const ta = document.getElementById("textArea");
+		ta.focus();
+		ta.select();
+		document.execCommand('copy');
+	})
+}
 
-  function copyToClipboard() {
-    setTimeout(() => {
-      const ta = document.getElementById("textArea");
-      ta.focus();
-      ta.select();
-      document.execCommand('copy');
-    })
-  }
-  
-  function prettifyXml(sourceXml) {
-    const xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
-    const xsltDoc = new DOMParser().parseFromString([
-        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
-        '  <xsl:output omit-xml-declaration="yes" indent="yes"/>',
-        '    <xsl:template match="node()|@*">',
-        '      <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
-        '    </xsl:template>',
-        '</xsl:stylesheet>',
-    ].join('\n'), 'application/xml');
+function prettifyXml (sourceXml) {
+	const xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
+	const xsltDoc = new DOMParser().parseFromString([
+		'<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+		'  <xsl:output omit-xml-declaration="yes" indent="yes"/>',
+		'    <xsl:template match="node()|@*">',
+		'      <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+		'    </xsl:template>',
+		'</xsl:stylesheet>',
+	].join('\n'), 'application/xml');
 
-    const xsltProcessor = new XSLTProcessor();    
-    xsltProcessor.importStylesheet(xsltDoc);
-    const resultDoc = xsltProcessor.transformToDocument(xmlDoc);
-    return new XMLSerializer().serializeToString(resultDoc);
+	const xsltProcessor = new XSLTProcessor();
+	xsltProcessor.importStylesheet(xsltDoc);
+	const resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+	return new XMLSerializer().serializeToString(resultDoc);
 };
-  
-  Vue.material.registerTheme('default', {
-    primary: 'blue',
-    'default': 'blue',
-    accent: 'red',
-    warn: 'red',
-    background: 'grey',
-    theme: 'dark'
-  })
 
-  var app = new Vue({
-  el: '#app',
-  data: {
-    textarea: '',
-    error: null
-  },
-  methods: {
-    safeExecute(fnc) {
-        this.error = null;
-        try {
-          fnc.call(this);
-          copyToClipboard();
-        } catch(e) {
-          console.error(e);
-          this.error = e;
-        }
-    },
 
-    encodeURL()  {
-      this.safeExecute(() => {
-        this.textarea = encodeURIComponent(this.textarea);
-      });
-    },
+// function encodeURL1 () {
+// 	alert('test');
+// }
 
-    decodeURL() {
-      this.safeExecute(() => {
-        this.textarea = decodeURIComponent(this.textarea);
-      });
-    },
+var app = new Vue({
+	el: '#app',
+	data () {
+		return {
+			textarea: '',
+			error: null,
+			actions: [
+				{label: 'encode URL', icon:'skip_next', action: this.encodeURL},
+				{label: 'decode URL', icon:'skip_previous', action: this.decodeURL},
+				{label: 'encode Base64', icon:'skip_next', action: this.encodeBase64},
+				{label: 'decode Base64', icon:'skip_previous', action: this.decodeBase64},
+				{label: 'format JSON', icon:'code', action: this.formatJson},
+				{label: 'format XML', icon:'code', action: this.formatXml}
+			]
+		}
+	},
+	methods: {
+		safeExecute (fnc) {
+			this.error = null;
+			try {
+				fnc.call(this);
+				copyToClipboard();
+			} catch (e) {
+				console.error(e);
+				this.error = e;
+			}
+		},
 
-    encodeBase64() {
-      this.safeExecute(() => {
-        this.textarea = btoa(this.textarea);
-      });
-    },
+		encodeURL () {
+			this.safeExecute(() => {
+				this.textarea = encodeURIComponent(this.textarea);
+			});
+		},
 
-    decodeBase64() {
-      this.safeExecute(() => {
-        this.textarea = atob(this.textarea);
-      });
-    },
-    
-    formatJson() {
-      this.safeExecute(() => {
-        this.textarea = JSON.stringify(JSON.parse(this.textarea), null, "\t");
-      });
-    },
-    
-    formatXml() {
-      this.safeExecute(() => {
-        this.textarea = prettifyXml(this.textarea);
-      });
-    }
-  }
+		decodeURL () {
+			this.safeExecute(() => {
+				this.textarea = decodeURIComponent(this.textarea);
+			});
+		},
+
+		encodeBase64 () {
+			this.safeExecute(() => {
+				this.textarea = btoa(this.textarea);
+			});
+		},
+
+		decodeBase64 () {
+			this.safeExecute(() => {
+				this.textarea = atob(this.textarea);
+			});
+		},
+
+		formatJson () {
+			this.safeExecute(() => {
+				this.textarea = JSON.stringify(JSON.parse(this.textarea), null, "\t");
+			});
+		},
+
+		formatXml () {
+			this.safeExecute(() => {
+				this.textarea = prettifyXml(this.textarea);
+			});
+		}
+	}
 })
