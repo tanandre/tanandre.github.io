0a21cd9698600ba5728c2e1331ca33b7115572f6
tanandre <tanneman1@gmail.com>
2018-04-29 16:53:24 -0400

    style google button

--- tools/js/script.js
+++ tools/js/script.js
@@ -12,6 +12,13 @@ function renderButton() {
 	});
 }
 
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
+
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
