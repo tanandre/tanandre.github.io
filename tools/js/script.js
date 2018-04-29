166079f29b80e1cb31f7b0901fdb7284101e92d3
tanandre <tanneman1@gmail.com>
2018-04-29 16:20:52 -0400

    add onSignIn for google sign in check

--- tools/js/script.js
+++ tools/js/script.js
@@ -1,5 +1,13 @@
 const KEY_AUTO_COPY = 'tanandre.github.io.tools.autoCopy';
 
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 function debounce (ms) {
 	let timer = 0;
 	return (callback) => {
