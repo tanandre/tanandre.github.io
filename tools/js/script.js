a6e9de883f3feb86ed447f74c89b46862aca47a3
tanandre <tanneman1@gmail.com>
2018-04-29 16:40:23 -0400

    add onSignIn for google sign in checkstyle google button

--- tools/js/script.js
+++ tools/js/script.js
@@ -1,5 +1,16 @@
 const KEY_AUTO_COPY = 'tanandre.github.io.tools.autoCopy';
 
+function renderButton() {
+	gapi.signin2.render('my-signin2', {
+		'scope': 'profile email',
+		'width': 240,
+		'height': 50,
+		'longtitle': true,
+		'theme': 'dark',
+		'onsuccess': onSuccess,
+		'onfailure': onFailure
+	});
+
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
